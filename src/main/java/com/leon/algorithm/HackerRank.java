59f9c8358d1ad7304b658066a3c0343c1cfcc2a6
leon <ntcong1312@gmail.com>
2018-01-01 21:39:43 +0700

    HackerRank: Some first exercises.

---
+++ src/main/java/com/leon/algorithm/HackerRank.java
@@ -0,0 +1,20 @@
+package com.leon.algorithm;
+
+public class HackerRank {
+
+    public static void main(String[] args) {
+        /*int x = 1, y = 1, z = 0;
+        if (!(x++ > 1 && y++ > 1)) {
+            z = x + y;
+        }
+
+        System.out.println(z);*/
+
+
+        /*int a = 5, b = 10;
+        int signal = ((a - b) >> 31) & 0x1;
+        int result = a - (signal * (a - b));
+        System.out.println(String.format("The higher number of %d and %d is %d", a, b, result));*/
+    }
+
+}
